b5ba5f6b7d01c9d358620fed93a881ff1f9bc7e4
Nikolasrralonso <nikolasrralonso@gmail.com>
2021-12-27 13:43:39 +0000

    Nasa Picture of the day using XMLHttpRequest

---
+++ random_image_nasa/script.js
@@ -0,0 +1,27 @@
+// Using XMLHttpRequest
+
+// API DATA
+const req = new XMLHttpRequest();
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const apiKey = 'DPKUZxs9bcV2gyDanj8mR6yBiVsySYOd679UhIgh'
+
+// HTML Selectors
+
+let title = document.getElementById('title')
+let date = document.getElementById('date')
+let descr = document.getElementById('description')
+let img = document.getElementById('image')
+
+req.open("GET", url+apiKey);
+req.send();
+
+req.addEventListener("load", function(){
+    if(req.status=200 && req.readyState==4){
+        let response = JSON.parse(req.responseText);
+        console.log(response)
+        title.textContent = response.title;
+        date.textContent = response.date;
+        img.src = response.hdurl;
+        descr.textContent = response.explanation;
+    }
+})
